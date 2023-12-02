c2f30a3bbf1db52348be7f31e68ef3743a1cfb82
Tom Ekander <ekander.tom@gmail.com>
2023-12-02 15:06:22 +0100

    2023 - Day 2 - open Base globally, get rid of exn

--- lib/problems/problem_2023_02.ml
+++ lib/problems/problem_2023_02.ml
@@ -1,9 +1,9 @@
+open Base
+
 let year = 2023
 let day = 2
 
 module Cube_conundrum = struct
-  open Base
-
   module Cube_count = struct
     type t =
       { red : int
@@ -12,6 +12,7 @@ module Cube_conundrum = struct
       }
 
     let create ~red ~green ~blue = { red; green; blue }
+    let empty () = create ~red:0 ~green:0 ~blue:0
   end
 
   module Game = struct
@@ -20,7 +21,7 @@ module Cube_conundrum = struct
       ; reveals : Cube_count.t list
       }
 
-    let create id reveals = { id; reveals }
+    let create ~id ~reveals = { id; reveals }
 
     let max_reveal_per_color t =
       t.reveals
@@ -30,7 +31,7 @@ module Cube_conundrum = struct
              ; green = max acc.green reveal.green
              ; blue = max acc.blue reveal.blue
              })
-           ~init:(Cube_count.create ~red:0 ~green:0 ~blue:0)
+           ~init:(Cube_count.empty ())
     ;;
 
     let is_playable t ~(available_cubes : Cube_count.t) =
@@ -41,65 +42,69 @@ module Cube_conundrum = struct
       && required_cubes.blue <= available_cubes.blue
     ;;
 
+    (* Parsing *)
     let parse_reveal s =
       String.split s ~on:','
       |> List.fold
-           ~f:(fun (acc : Cube_count.t) reveal ->
+           ~f:(fun (cubes : Cube_count.t) reveal ->
              match reveal |> String.strip |> String.split ~on:' ' with
-             | [ cubes; "red" ] -> { acc with red = Int.of_string cubes }
-             | [ cubes; "green" ] -> { acc with green = Int.of_string cubes }
-             | [ cubes; "blue" ] -> { acc with blue = Int.of_string cubes }
-             | _ -> acc)
-           ~init:(Cube_count.create ~red:0 ~green:0 ~blue:0)
+             | [ count; "red" ] -> { cubes with red = Int.of_string count }
+             | [ count; "green" ] -> { cubes with green = Int.of_string count }
+             | [ count; "blue" ] -> { cubes with blue = Int.of_string count }
+             | _ -> cubes)
+           ~init:(Cube_count.empty ())
     ;;
 
     let parse s =
       match String.split s ~on:':' with
-      | [ id; reveals ] ->
-        let id = String.split id ~on:' ' |> List.last_exn |> Int.of_string in
-        let reveals = reveals |> String.split ~on:';' |> List.map ~f:parse_reveal in
+      | [ id; reveals ] -> begin
+        match String.split id ~on:' ' with
+        | [ "Game"; id ] ->
+          let id = Int.of_string id in
+          let reveals = reveals |> String.split ~on:';' |> List.map ~f:parse_reveal in
 
-        Some (create id reveals)
+          Some (create ~id ~reveals)
+        | _ -> None
+      end
       | _ -> None
     ;;
   end
+end
 
-  (* Part 1 *)
-  let solve input =
+module Part_1 = struct
+  open Cube_conundrum
+
+  let run (input : string) : (string, string) Result.t =
     let bag = Cube_count.create ~red:12 ~green:13 ~blue:14 in
 
-    let playable_games =
+    let ids_summed =
       input
       |> String.split ~on:'\n'
       |> List.filter_map ~f:Game.parse
       |> List.filter ~f:(Game.is_playable ~available_cubes:bag)
+      |> List.fold ~f:(fun sum ({ id; _ } : Game.t) -> sum + id) ~init:0
     in
 
-    playable_games |> List.fold ~f:(fun sum ({ id; _ } : Game.t) -> sum + id) ~init:0
-  ;;
-
-  (* Part 2 *)
-  let solve_2 input =
-    let games = input |> String.split ~on:'\n' |> List.filter_map ~f:Game.parse in
-
-    games
-    |> List.fold
-         ~f:(fun sum (game : Game.t) ->
-           let cubes = Game.max_reveal_per_color game in
-
-           sum + (cubes.red * cubes.green * cubes.blue))
-         ~init:0
-  ;;
-end
-
-module Part_1 = struct
-  let run (input : string) : (string, string) result =
-    Ok (Cube_conundrum.solve input |> Int.to_string)
+    Ok (Int.to_string ids_summed)
   ;;
 end
 
 module Part_2 = struct
-  let run (input : string) : (string, string) result =
-    Ok (Cube_conundrum.solve_2 input |> Int.to_string)
+  open Cube_conundrum
+
+  let run (input : string) : (string, string) Result.t =
+    let sum =
+      input
+      |> String.split ~on:'\n'
+      |> List.filter_map ~f:Game.parse
+      |> List.fold
+           ~f:(fun sum (game : Game.t) ->
+             let cubes = Game.max_reveal_per_color game in
+
+             sum + (cubes.red * cubes.green * cubes.blue))
+           ~init:0
+    in
+
+    Ok (Int.to_string sum)
   ;;
 end
